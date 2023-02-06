e63d1bc970e9294a4a595e1b9a563c8dc7f43203
milind panwar <milind.panwar@jarvis.consulting>
2023-02-06 12:27:46 +0530

    added view icon in reels

---
+++ lib/Dashboard/Screen/socialMedia/reels/likebutton/cubit/LikeButtonCubit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'LikeButtonState.dart';
+
+class LikeButtonCubit extends Cubit<LikeButtonState> {
+  LikeButtonCubit() : super(LikeButtonInitialState());
+
+  Future postLikeData() async {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+}
